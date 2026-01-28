303cfecb3197171b923c3d3a87b7078c329c32ad
Oscar <oscar@candidatic.com>
2026-01-28 00:03:21 -0600

    fix: restore missing 'positions' constant in Toast component

--- src/components/ui/Toast.jsx
+++ src/components/ui/Toast.jsx
@@ -48,6 +48,14 @@ const Toast = ({
         },
     };
 
+    const positions = {
+        'top-right': 'top-4 right-4',
+        'top-left': 'top-4 left-4',
+        'bottom-right': 'bottom-4 right-4',
+        'bottom-left': 'bottom-4 left-4',
+        'top-center': 'top-4 left-1/2 transform -translate-x-1/2',
+    };
+
     const config = types[type] || types.info;
     const Icon = config.icon;
 
