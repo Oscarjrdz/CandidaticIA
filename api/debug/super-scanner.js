a7347826f149206f9811837500ac4a22e2f6a182
Oscar <oscar@candidatic.com>
2026-02-22 08:23:04 -0600

    cleanup: remove additional debug endpoints

--- api/debug/super-scanner.js
+++
@@ -1,6 +0,0 @@
-import { getProjectById } from '../utils/storage.js';
-
-export default async function handler(req, res) {
-    const proj = await getProjectById('proj_1771225156891_10ez5k');
-    res.status(200).json(proj);
-}
